bb49e4f43c6404a4a8c5e684c663809dfdae94d5
zihong <zihong87@gmail.com>
2015-12-03 19:01:30 +0800

    add callback to js in KCJSCallback

--- kerkee/src/com/kercer/kerkee/bridge/type/KCJSCallback.java
+++ kerkee/src/com/kercer/kerkee/bridge/type/KCJSCallback.java
@@ -3,6 +3,9 @@ package com.kercer.kerkee.bridge.type;
 import com.kercer.kerkee.bridge.KCJSExecutor;
 import com.kercer.kerkee.webview.KCWebView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class KCJSCallback implements KCJSType
 {
 	private String mCallbackId;
@@ -16,12 +19,27 @@ public class KCJSCallback implements KCJSType
 	{
 		return mCallbackId;
 	}
-	
+
+
+	public void callbackToJS(KCWebView aWebView)
+	{
+		KCJSExecutor.callbackJS(aWebView, mCallbackId);
+	}
+
 	public void callbackToJS(KCWebView aWebView, String aStr)
 	{
 		KCJSExecutor.callbackJS(aWebView, mCallbackId, aStr);
 	}
-	
+
+	public void callbackToJS(KCWebView aWebView, JSONObject aJSONObject)
+	{
+		KCJSExecutor.callbackJS(aWebView, mCallbackId, aJSONObject);
+	}
+
+	public void callbackToJS(KCWebView aWebView, JSONArray aJSONArray)
+	{
+		KCJSExecutor.callbackJS(aWebView, mCallbackId, aJSONArray);
+	}
 	
 	public String toString()
 	{
