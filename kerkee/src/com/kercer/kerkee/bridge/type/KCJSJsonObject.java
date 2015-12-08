3d68eb7fb441bc5c66e94876cd8ddccf8c88b5f2
zihong <zihong87@gmail.com>
2015-12-08 14:32:24 +0800

    add KCJSJsonObject construction

--- kerkee/src/com/kercer/kerkee/bridge/type/KCJSJsonObject.java
+++ kerkee/src/com/kercer/kerkee/bridge/type/KCJSJsonObject.java
@@ -1,8 +1,32 @@
 package com.kercer.kerkee.bridge.type;
 
+import org.json.JSONObject;
+
+import java.util.Map;
+
 /**
  * Created by zihong on 15/12/3.
  */
 public class KCJSJsonObject implements KCJSType
 {
+    JSONObject mJsonObject;
+
+    public KCJSJsonObject()
+    {
+        mJsonObject = new JSONObject();
+    }
+    public KCJSJsonObject(JSONObject aJsonObject)
+    {
+        mJsonObject = aJsonObject;
+    }
+
+    public KCJSJsonObject(Map aCopyFrom)
+    {
+        mJsonObject = new JSONObject(aCopyFrom);
+    }
+
+    public String toString()
+    {
+        return mJsonObject != null ? mJsonObject.toString() : null;
+    }
 }
