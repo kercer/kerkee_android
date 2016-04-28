357e56201bc6dfc26430e5415f8c98565d8574ba
zihong <zihong87@gmail.com>
2016-04-28 12:17:29 +0800

    add some function in KCEvent

--- kerkee/src/com/kercer/kerkee/bridge/event/KCEvent.java
+++ kerkee/src/com/kercer/kerkee/bridge/event/KCEvent.java
@@ -1,7 +1,9 @@
 package com.kercer.kerkee.bridge.event;
 
+import com.kercer.kercore.util.KCUtilText;
 import com.kercer.kerkee.bridge.KCArgList;
 import com.kercer.kerkee.bridge.KCJSDefine;
+import com.kercer.kerkee.bridge.type.KCJSCallback;
 import com.kercer.kerkee.webview.KCWebView;
 
 import java.util.HashMap;
@@ -27,13 +29,45 @@ public class KCEvent
 		return mEvents;
 	}
 
+	public static KCJSCallback getCallback(String aEvent)
+	{
+		KCJSCallback callback = null;
+		if (!KCUtilText.isEmpty(aEvent))
+		{
+			Integer callbackID = mEvents.get(aEvent);
+			callback = new KCJSCallback(callbackID.toString());
+		}
+		return callback;
+	}
 
 	public static boolean hasEvent(String aEvent)
 	{
 		return mEvents.containsKey(aEvent);
 	}
 
+	public static boolean callEvent(KCWebView aWebView, String aEvent, Object... aArgs)
+	{
+		boolean isOk = false;
+		KCJSCallback callback = getCallback(aEvent);
+		if (callback != null)
+		{
+			callback.callbackToJS(aWebView, aArgs);
+			isOk = true;
+		}
+		return isOk;
+	}
 
+	public static boolean callEvent(KCWebView aWebView, String aEvent)
+	{
+		boolean isOk = false;
+		KCJSCallback callback = getCallback(aEvent);
+		if (callback != null)
+		{
+			callback.callbackToJS(aWebView);
+			isOk = true;
+		}
+		return isOk;
+	}
 
 
 	/*****************************************************
