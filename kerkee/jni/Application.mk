b10216cc6b2ece7f9f42e7d0767917dfa99be4fc
cai <cai353974361@163.com>
2019-04-17 13:38:40 +0800

    升级gradle  跳转ndk配置（移除多余的so）

--- kerkee/jni/Application.mk
+++ kerkee/jni/Application.mk
@@ -1,2 +1,4 @@
-APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
-APP_PLATFORM := android-8
+#APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_PLATFORM := android-14
+APP_ALLOW_MISSING_DEPS=true
